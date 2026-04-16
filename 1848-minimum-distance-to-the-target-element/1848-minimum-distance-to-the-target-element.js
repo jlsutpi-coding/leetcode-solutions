0c175048cf6d57cf886c1cde338ab479e637906a
Jl Sut Pi <sutpi070@gmail.com>
2026-04-16 14:22:42 +0700

    Time: 0 ms (100%), Space: 54.3 MB (26.63%) - LeetHub

---
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @param {number} start
+ * @return {number}
+ */
+var getMinDistance = function (nums, target, start) {
+    let res = nums.length;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            res =  Math.min(res, Math.abs(i - start));
+        }
+    }
+    return res;
+};

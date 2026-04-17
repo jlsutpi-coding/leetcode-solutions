a2c8eee125fa24808a756fcd873286917f12dba1
Jl Sut Pi <sutpi070@gmail.com>
2026-04-17 13:52:18 +0700

    Time: 55 ms (5.54%), Space: 53.7 MB (55.55%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    const result = [];
+
+    for(let i = 0; i < arr.length; i++ ) {
+        
+        fn(arr[i],i) ? result.push(arr[i]) : result;
+    }
+
+    return result;
+};
